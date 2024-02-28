2fda056b38a50f59c1872a75d0ec350f5dab0948
jowens4 <jowens@avidpd.com>
2024-02-28 16:22:38 -0700

    working, got users added to a database

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:navi_app/login.dart';
 import 'package:navi_app/my_app_bar.dart';
 import 'package:provider/provider.dart';
 import 'auth_service.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 enum AppScreen { login, home, details, data }
 
@@ -172,10 +174,80 @@ class LoginScreen extends StatelessWidget {
 }
 
 // Replace these with your actual screens
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  Future<void> createUser(String email, String password) async {
+    final String apiUrl = 'http://aqp:8080/users/';
+
+    Map<String, String> requestBody = {
+      'email': email,
+      'password': password,
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: <String, String>{
+          'accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(requestBody),
+      );
+
+      if (response.statusCode == 200) {
+        print("User created successfully");
+      } else {
+        print("Error creating user. Status code: ${response.statusCode}");
+        print("Response body: ${response.body}");
+      }
+    } catch (e) {
+      print("Error during POST request: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Home Screen');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter POST Request"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                createUser(emailController.text, passwordController.text);
+              },
+              child: Icon(Icons.send),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
